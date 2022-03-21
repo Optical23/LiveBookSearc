4b7caaab92bf3b873e43f5080a2ae6420b2beb54
Optical23 <aaron@thefergusons.com>
2022-03-21 18:14:05 -0500

    bug fix for express path

--- server/server.js
+++ server/server.js
@@ -10,8 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use(routes);
